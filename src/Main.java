c5b3c0c732bf517cb92d709be80bb8170e3a06c3
Николай <kolya-91@list.ru>
2024-02-22 12:19:00 +0500

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,14 @@ public class Main {
         // Задача 2
         System.out.println(" Задача 2");
 
-        int clientDeviceYear = 2014;
+        int clientDeviceYear = 2016;
         if (clientDeviceYear > 2015 && clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientDeviceYear <= 2015 && clientOS == 1 ); {
+        } else if (clientDeviceYear <= 2015 && clientOS == 1 ) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } if (clientDeviceYear > 2015 && clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear <= 2015 && clientOS == 0);{
+        } else if (clientDeviceYear <= 2015 && clientOS == 0) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
         // Задача 3
@@ -34,6 +34,62 @@ public class Main {
         }
         // Задача 4
         System.out.println(" Задача 4");
+        int deliveryDistance = 110;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуются сутки на доставку");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется двое суток на доставку");
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            System.out.println(" Потребуются трое суток на доставку");
+        } else {
+            System.out.println(" Доставка невозможна");
+        }
+        // Задача 5
+        System.out.println(" Задача 5");
+        int monthNumber = 4;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Нет больше вариантов");
+        }}}
 
-    }
-}
+
+
+
+
+
